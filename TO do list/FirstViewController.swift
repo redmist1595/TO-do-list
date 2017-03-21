7858893b7e84c7ff7abb48dba1b69f31d8290567
Abhinav <abhinavpaliwal@Abhinavs-MacBook-Pro.local>
2017-03-22 01:34:52 +0400

    code

--- TO do list/FirstViewController.swift
+++ TO do list/FirstViewController.swift
@@ -8,12 +8,91 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    @IBOutlet var table: UITableView!
+    
+    var items: [String] = []
+
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return items.count
+        
+    }
+    
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    
+    {
+        var cellLabel = " "
+        
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        
+        if let tempLabel = items[indexPath.row] as? String
+        {
+            
+            cellLabel = tempLabel
+        }
+        
+        cell.textLabel?.text = cellLabel
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+        
+        }
+    
+    override func viewDidAppear(_ animated: Bool)
+    
+    {
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        
+        
+        if let tempitems = itemsObject as? [String]
+        {
+            
+            
+            items = tempitems
+            
+            }
+
+        table.reloadData()
+        
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete
+        
+        
+            {
+             items.remove(at: indexPath.row)
+                
+                table.reloadData()
+                
+                UserDefaults.standard.set(items, forKey: "items")
+                
+            }
+    }
+    
+    
+  /*  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            numbers.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
+    
+    */
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- TO do list/SecondViewController.swift
+++ TO do list/SecondViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+ class SecondViewController: UIViewController, UITextFieldDelegate {
 
-class SecondViewController: UIViewController {
+    @IBOutlet var itemTextField: UITextField!
+    
+    @IBAction func add(_ sender: Any) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        
+        var items: [String]
+        
+        if let tempitems = itemsObject as? [String]
+        {
+            items = tempitems
 
+        
+            items.append(itemTextField.text!)
+            
+            
+        }
+        
+        else{
+             items = [itemTextField.text!]
+        }
+        
+        UserDefaults.standard.set(items, forKey: "items")
+        
+        itemTextField.text = " "
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
